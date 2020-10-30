9681b009b269c5d16e9bcdb4cf1b09288da01837
David <DavidLegrand@users.noreply.github.com>
2020-10-30 15:11:26 +0100

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  isAuth = false
+  constructor(){
+    setTimeout(()=>{
+      this.isAuth = true
+    },2000)
+  }
 }
