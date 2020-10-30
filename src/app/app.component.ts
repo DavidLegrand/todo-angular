c92fa8522d0560a06f6dabcf7ae80d7943a55086
David <DavidLegrand@users.noreply.github.com>
2020-10-30 15:31:23 +0100

    Décorateur @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isAuth = false
-  constructor(){
-    setTimeout(()=>{
-      this.isAuth = true
-    },2000)
+  isAuth = false;
+  taskName1 = 'Finaliser les maquettes';
+  taskName2 = 'Intégrer le module de paiement';
+  taskName3 = "Développer l'authentification";
+
+  taskComplete1 = false
+  taskComplete2 = true
+  taskComplete3 = false
+  
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 2000);
   }
-  onComplete(){
-    console.log("All tasks are completed !")
+  onComplete() {
+    console.log('All tasks are completed !');
   }
 }
