f6999d22da3e4cfd049cee48a475c811fe74a3a3
David <DavidLegrand@users.noreply.github.com>
2020-11-02 16:05:35 +0100

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isAuth = false
-  constructor(){
-    setTimeout(()=>{
-      this.isAuth = true
-    },2000)
+  isAuth: boolean = false;
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 2000);
   }
 }
